493905d268faa416684cd06e8efab5cc163b90f9
Diego Obin <diego.obin23@gmail.com>
2021-09-07 12:45:03 -0600

    Commit No. 1

---
+++ log/Ejecutable.sh
@@ -0,0 +1,8 @@
+#userid -- ORACLE username/password@IP:PUERTO/SID -> DiiAns23/andres23@
+#control -- control file name
+#log -- log file name
+#bad -- bad file name
+sqlldr userid=DiiAns23/andres23@34.71.134.122:1521/ORCL18 control=ArchivoControl.ctl
+echo " "
+echo -e "\e[96m  ENTER PARA CONTINUAR ... \e[0m"
+read
---
+++ log/InstruccionesDDL.sql
@@ -0,0 +1,4 @@
+DROP TABLE ciudad;
+DROP TABLE pais;
+DROP TABLE 
+COMMIT;
