3facffc2f87b9b368e48b7163a63ce154588a42e
Diego Obin <diego.obin23@gmail.com>
2021-09-09 21:33:30 -0600

    Commit No. 2
    Cosas nuevas

--- log/Ejecutable.sh
+++ log/Ejecutable.sh
@@ -2,7 +2,7 @@
 #control -- control file name
 #log -- log file name
 #bad -- bad file name
-sqlldr userid=DiiAns23/andres23@34.71.134.122:1521/ORCL18 control=ArchivoControl.ctl
+sqlldr userid=DiiAns23/andres23@34.69.108.82:1521/ORCL18 control=ArchivoControl.ctl
 echo " "
 echo -e "\e[96m  ENTER PARA CONTINUAR ... \e[0m"
 read
